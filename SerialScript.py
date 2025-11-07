fe055bad2ee4a8248fab7fbb21917ee38f033447
Rino6357 <60277295+Rino6357@users.noreply.github.com>
2025-11-06 21:18:25 -0500

    Create SerialScript.py

---
+++ SerialScript.py
@@ -0,0 +1,24 @@
+import serial
+import pyautogui
+
+ser = serial.Serial('COM5', 9600)
+
+while True:
+    if ser.in_waiting > 0:
+        data = ser.readline().decode('uft-8').rstrip()
+
+        if data == 'u':
+            pyautogui.press('up')
+            print('success')
+
+        elif data == 'd':
+            pyautogui.press('down')   
+            print('success')
+        
+        elif data == 'l':
+            pyautogui.press('left')  
+            print('success')
+
+        elif data == 'r':
+            pyautogui.press('right')  
+            print('success')
